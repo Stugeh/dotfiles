c9895beae43dbcde817ee7e93c578e8ba2d75b7f
tuukka veteli <tuukka.veteli@solita.fi>
2023-04-16 00:50:09 +0300

    deleted some fonts

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
-picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
+~/.config/defaultScreenLayout.sh &
+picom --vsync -b &
+feh --bg-fill ~/Pictures/forest-4k-desktop-background-hd-wallpaper-c816fdd8e0505ca8000cb11eb862c48a.jpg &
+dunst &
+killall volumeicon & 
+volumeicon & 
+nm-applet & 
+~/.config/polybar/launch.sh
 
-# Low battery notifier
-~/.config/qtile/scripts/check_battery.sh & disown
-
-# Start welcome
-eos-welcome & disown
-
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
