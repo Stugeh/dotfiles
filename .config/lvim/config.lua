348818e7c439b02cabddf679ce8157a7a9071c44
tuukka veteli <tuukkaveteli@gmail.com>
2023-09-13 21:09:21 +0300

    updated lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -7,6 +7,9 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.relativenumber = true
 vim.opt.timeoutlen = 150
+vim.opt.guifont = { "LiterationMono Nerd Font:h11" }
+vim.g.neovide_transparency = 0.8
+
 -- general
 lvim.log.level = "info"
 lvim.format_on_save = true
@@ -111,7 +114,36 @@ lvim.plugins = {
 	{ "sainnhe/gruvbox-material" },
 	{ "sainnhe/everforest" },
 	{ "catppuccin/nvim", name = "catppuccin" },
-
+	{
+		"ggandor/lightspeed.nvim",
+		event = "BufRead",
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"mrjones2014/nvim-ts-rainbow",
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rub_fn, hsl_fn
+			})
+		end,
+	},
+	{
+		"tpope/vim-surround",
+	},
 	-- RUST
 	{
 		"simrat39/rust-tools.nvim",
