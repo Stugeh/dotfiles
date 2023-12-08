a6658cd538cc5c3c0b31e0063ba2623a1b54af1d
tuukka veteli <tuukkaveteli@gmail.com>
2023-12-08 22:17:04 +0200

    added binding to open ranger

--- .config/nvim/lua/plugs/toggleterm.lua
+++ .config/nvim/lua/plugs/toggleterm.lua
@@ -5,4 +5,14 @@ return {
     direction = 'float',
     open_mapping = [[<c-\>]],
   },
+  config = function()
+    local Terminal = require('toggleterm.terminal').Terminal
+    local ranger   = Terminal:new({ cmd = "ranger ~", hidden = true, direction = 'float' })
+
+    function _ranger_toggle()
+      ranger:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>\\r", "<cmd>lua _ranger_toggle()<CR>", { noremap = true, silent = true })
+  end
 }
