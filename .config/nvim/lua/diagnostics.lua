faf642f4de4558cc3a653da85856ce1a7224a7a8
Tuukka Veteli <tuukka.veteli@solita.fi>
2024-02-24 01:27:24 +0200

    cleaned up diagnostics setup

--- .config/nvim/lua/diagnostics.lua
+++ .config/nvim/lua/diagnostics.lua
@@ -1,31 +1,29 @@
-ErrorIcon = '✘'
-WarningIcon = '▲'
-HintIcon = ''
-InfoIcon = ''
-
-vim.diagnostic.config {
-  severity_sort = true,
+local icons = require 'icons'
+local diagnostic_config = {
   signs = {
     active = true,
     values = {
-      [vim.diagnostic.severity.ERROR] = ErrorIcon,
-      [vim.diagnostic.severity.WARN] = WarningIcon,
-      [vim.diagnostic.severity.HINT] = HintIcon,
-      [vim.diagnostic.severity.INFO] = InfoIcon,
+      { name = 'DiagnosticSignError', text = icons.diagnostics.Error },
+      { name = 'DiagnosticSignWarn', text = icons.diagnostics.Warning },
+      { name = 'DiagnosticSignHint', text = icons.diagnostics.Hint },
+      { name = 'DiagnosticSignInfo', text = icons.diagnostics.Information },
     },
   },
+  virtual_text = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = true,
+    style = 'minimal',
+    border = 'rounded',
+    source = 'always',
+    header = '',
+    prefix = '',
+  },
 }
+vim.diagnostic.config(diagnostic_config)
 
--- Set gutter icons
-local sign = function(opts)
-  vim.fn.sign_define(opts.name, {
-    texthl = opts.name,
-    text = opts.text,
-    numhl = '',
-  })
+for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), 'signs', 'values') or {}) do
+  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
 end
-
-sign { name = 'DiagnosticSignInfo', text = InfoIcon }
-sign { name = 'DiagnosticSignHint', text = HintIcon }
-sign { name = 'DiagnosticSignWarn', text = WarningIcon }
-sign { name = 'DiagnosticSignError', text = ErrorIcon }
