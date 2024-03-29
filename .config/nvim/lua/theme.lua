dabc0b6bb52114379aaef9541c1edd75873038e5
Tuukka Veteli <tuukka.veteli@solita.fi>
2024-03-29 21:28:31 +0200

    added cappuccin and changed StatusLine bg color

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -3,9 +3,10 @@ return {
   -- require 'themes.gruvpuccin',
   -- require 'themes.nightfox',
   -- require 'themes.github',
+  -- require 'themes.catpuccin',
 
   -- Overrides
-  vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'black', fg = 'white' }),
+  vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'orange', fg = 'white' }),
   -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'black', fg = 'lightblue' })
   -- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'black' })
   -- vim.api.nvim_set_hl(0, 'MasonHeader', { bg = 'black', fg = 'white' })
